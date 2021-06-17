dbfa34c677b2b7acb32f89782ea168ca2a1158f1
markliu2013 <markliu2013@gmail.com>
2021-06-17 14:36:51 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Mingwei Liu on 2021/6/17.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
